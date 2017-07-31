e07382ccdcb42a63bf5480d83a61f7af8b89d819
Peter Kazanzides <pkaz@jhu.edu>
2017-07-30 22:19:12 -0400

    Removed duplicate entries in GenMCS.cmd and GenMCSEth.cmd

--- FPGA1394_QLA/Generated/GenMCSEth.cmd
+++ FPGA1394_QLA/Generated/GenMCSEth.cmd
@@ -1,14 +1,9 @@
 setMode -pff
-setMode -pff
 addConfigDevice  -name "FPGA1394Eth-QLA" -path "."
 setSubmode -pffspi
 setAttribute -configdevice -attr multibootBpiType -value ""
 addDesign -version 0 -name "0"
-setMode -pff
 addDeviceChain -index 0
-setMode -pff
-addDeviceChain -index 0
-setAttribute -configdevice -attr compressed -value "FALSE"
 setAttribute -configdevice -attr compressed -value "FALSE"
 setAttribute -configdevice -attr autoSize -value "FALSE"
 setAttribute -configdevice -attr fileFormat -value "mcs"
@@ -16,20 +11,10 @@ setAttribute -configdevice -attr fillValue -value "FF"
 setAttribute -configdevice -attr swapBit -value "FALSE"
 setAttribute -configdevice -attr dir -value "UP"
 setAttribute -configdevice -attr multiboot -value "FALSE"
-setAttribute -configdevice -attr multiboot -value "FALSE"
 setAttribute -configdevice -attr spiSelected -value "TRUE"
-setAttribute -configdevice -attr spiSelected -value "TRUE"
-addPromDevice -p 1 -size 2048 -name 2M
-setMode -pff
-addDeviceChain -index 0
-setMode -pff
-addDeviceChain -index 0
-setSubmode -pffspi
-setMode -pff
+addPromDevice -p 1 -size 2048
 setAttribute -design -attr name -value "0000"
 addDevice -p 1 -file "../FPGA1394EthQLA.bit"
-setMode -pff
-setSubmode -pffspi
 generate
 setCurrentDesign -version 0
 exit
