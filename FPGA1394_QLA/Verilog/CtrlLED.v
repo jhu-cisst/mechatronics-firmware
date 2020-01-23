ce57282f410af8e2ccaa6c310f2ad845339b6fdb
Peter Kazanzides <pkaz@jhu.edu>
2020-01-22 22:20:54 -0500

    Refactored LED code to avoid duplication

--- FPGA1394_QLA/Verilog/CtrlLED.v
+++ FPGA1394_QLA/Verilog/CtrlLED.v
@@ -34,21 +34,60 @@ begin
     end
 end
 
+reg[31:0] ROM_PWM[0:1023];    // pwm period lookup table
+
+// Initialization
+integer i;
+initial begin
+    // initialize lookup table here
+    for (i=  0; i<  1; i=i+1) ROM_PWM[i] = 20; // first element
+    for (i=  1; i<100; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] + 20;
+    for (i=100; i<200; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] - 20;
+    for (i=200; i<300; i=i+1) ROM_PWM[i] = 0;
+    for (i=300; i<400; i=i+1) ROM_PWM[i] = 0;
+    for (i=400; i<500; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] + 20;
+    for (i=500; i<600; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] - 20;
+    for (i=600; i<700; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] + 20;
+    for (i=700; i<800; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] - 20;
+    for (i=800; i<900; i=i+1) ROM_PWM[i] = 0;
+    for (i=900; i<=1023; i=i+1) ROM_PWM[i] = 0;
+end
+
+// generate clocks
+wire clk_768khz;  // clk 768khz
+ClkDiv div768khz(sysclk, clk_768khz); defparam div768khz.width = 6;
+wire clk_pwm;  // pwm signal 187.5 Hz, 4096 cnts clk_768khz
+ClkDiv divpwm(sysclk, clk_pwm); defparam divpwm.width = 18;
+wire clk_pwm_width;  // clk for pwm width update 93.75 Hz
+ClkDiv divpw(sysclk, clk_pwm_width); defparam divpw.width = 19;
+
 // show green 1, red 1, green 2, red 2 leds using pwm
 wire led1_grn_pwm;
-LEDPWM led_green_pwm_1(sysclk, led1_grn_pwm);
+wire[9:0] led1_grn_period;
+wire[31:0] led1_grn_pwm_value;
+assign led1_grn_pwm_value = ROM_PWM[led1_grn_period];
+LEDPWM led_green_pwm_1(clk_768khz, clk_pwm, clk_pwm_width, led1_grn_pwm, led1_grn_period, led1_grn_pwm_value);
 defparam led_green_pwm_1.start_phase = 250;
 
 wire led1_red_pwm;
-LEDPWM led_red_pwm_1(sysclk, led1_red_pwm);
+wire[9:0] led1_red_period;
+wire[31:0] led1_red_pwm_value;
+assign led1_red_pwm_value = ROM_PWM[led1_red_period];
+LEDPWM led_red_pwm_1(clk_768khz, clk_pwm, clk_pwm_width, led1_red_pwm, led1_red_period, led1_red_pwm_value);
 defparam led_red_pwm_1.start_phase = 700;
 
 wire led2_grn_pwm;
-LEDPWM led_green_pwm_2(sysclk, led2_grn_pwm);
+wire[9:0] led2_grn_period;
+wire[31:0] led2_grn_pwm_value;
+assign led2_grn_pwm_value = ROM_PWM[led2_grn_period];
+LEDPWM led_green_pwm_2(clk_768khz, clk_pwm, clk_pwm_width, led2_grn_pwm, led2_grn_period, led2_grn_pwm_value);
 defparam led_green_pwm_2.start_phase = 0;
 
 wire led2_red_pwm;
-LEDPWM led_red_pwm_2(sysclk, led2_red_pwm);
+wire[9:0] led2_red_period;
+wire[31:0] led2_red_pwm_value;
+assign led2_red_pwm_value = ROM_PWM[led2_red_period];
+LEDPWM led_red_pwm_2(clk_768khz, clk_pwm, clk_pwm_width, led2_red_pwm, led2_red_period, led2_red_pwm_value);
 defparam led_red_pwm_2.start_phase = 200;
 
 // output to led using mux
--- FPGA1394_QLA/Verilog/LEDPWM.v
+++ FPGA1394_QLA/Verilog/LEDPWM.v
@@ -10,25 +10,21 @@
  */
 
 module LEDPWM(
-    input wire sysclk,
-    output reg led_drive
+    input wire clk_768khz,
+    input wire clk_pwm,
+    input wire clk_pwm_width,
+    output reg led_drive,
+    output reg[9:0] period,      // for cycling through lookup table
+    input wire[31:0] pwm_value   // PWM value from lookup table
     );
 
 // define parameter
 parameter start_phase = 0;   // pwm param, 0 to 1023 
-
-// generate clock
-wire clk_768khz;  // clk 768khz
-ClkDiv div768khz(sysclk, clk_768khz); defparam div768khz.width = 6;
-wire clk_pwm;  // pwm signal 187.5 Hz, 4096 cnts clk_768khz
-ClkDiv divpwm(sysclk, clk_pwm); defparam divpwm.width = 18;
-wire clk_pwm_width;  // clk for pwm width update 93.75 Hz
-ClkDiv divpw(sysclk, clk_pwm_width); defparam divpw.width = 19;
+// initialize period to start_phase
+initial period = start_phase;
 
 // variable
-reg[31:0] ROM_PWM[0:1023];    // pwm period lookup table
-reg[31:0] period,i;    // for cycling through lookup table
-reg[31:0] pulse_width;   // current pwm width 
+reg[31:0] pulse_width;  // current pwm width
 reg[31:0] count_width;  // pulse width counter
 
 // Update led_drive based on pwm width
@@ -45,28 +41,8 @@ end
 // update pwm width at 93.75 Hz
 always @(posedge(clk_pwm_width))
 begin
-    period <= (period+1) % 1023;
-    pulse_width <= ROM_PWM[period];
-end
-
-
-// Initialization
-initial begin
-    // initialize lookup table here
-    for (i=  0; i<  1; i=i+1) ROM_PWM[i] = 20; // first element
-    for (i=  1; i<100; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] + 20;
-    for (i=100; i<200; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] - 20;
-    for (i=200; i<300; i=i+1) ROM_PWM[i] = 0;
-    for (i=300; i<400; i=i+1) ROM_PWM[i] = 0;
-    for (i=400; i<500; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] + 20;
-    for (i=500; i<600; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] - 20;
-    for (i=600; i<700; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] + 20;
-    for (i=700; i<800; i=i+1) ROM_PWM[i] = ROM_PWM[i-1] - 20;
-    for (i=800; i<900; i=i+1) ROM_PWM[i] = 0;
-    for (i=900; i<=1023; i=i+1) ROM_PWM[i] = 0;
-
-    // initialize period to start_phase
-    period = start_phase;
+    period <= period + 10'd1;  // 0-1023
+    pulse_width <= pwm_value;  // pwm_value = ROM_PWM[period]
 end
 
 endmodule
