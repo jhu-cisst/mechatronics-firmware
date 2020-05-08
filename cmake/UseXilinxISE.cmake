d90c8dc7f7fb6792f0f3f0b451ab8fbc361ccdad
Peter Kazanzides <pkaz@jhu.edu>
2020-05-08 02:31:31 -0400

    Changed CMake implementation to use XFLOW and custom OPT files instead of individual executables

--- cmake/UseXilinxISE.cmake
+++ cmake/UseXilinxISE.cmake
@@ -25,14 +25,109 @@
 # PROJ_NAMES are FPGA1394QLA and FPGA1394EthQLA, while the corresponding PROJ_OUTPUT
 # are FPGA1394-QLA and FPGA1394Eth-QLA.
 #
-# This macro could be enhanced to accept parameters that provide options for
+# There are two implementations:
+#   - ise_compile_fpga:      this uses Xilinx XFLOW to do most of the work
+#   - ise_compile_fpga_old:  this calls the individual executable programs
+#
+# For ise_compile_fpga, the command line options are in "opt" files that are copied
+# from this directory to the build directory.
+#
+# For ise_compile_fpga_old, the command line options are included below.
+#
+# In both cases, the macros could be enhanced to accept parameters that provide options for
 # the various compilation steps. Presently, most options are left at their default
 # values, with a few exceptions noted. These settings are consistent with the ISE
 # project files, FPGA1394-QLA.xise and FPGA1394Eth-QLA.xise.
-#
-# NOTE: This macro would be a lot simpler using XFLOW
 
 macro (ise_compile_fpga ...)
+
+  # set all keywords and their values to ""
+  set (FUNCTION_KEYWORDS
+       PROJ_NAME
+       DEPENDENCIES
+       FPGA_PARTNUM
+       VERILOG_SOURCE
+       UCF_FILE
+       IPCORE_DIR
+       TOP_LEVEL
+       PROJ_OUTPUT)
+
+  # reset local variables
+  foreach(keyword ${FUNCTION_KEYWORDS})
+    set (${keyword} "")
+  endforeach(keyword)
+
+  # parse input
+  foreach (arg ${ARGV})
+    list (FIND FUNCTION_KEYWORDS ${arg} ARGUMENT_IS_A_KEYWORD)
+    if (${ARGUMENT_IS_A_KEYWORD} GREATER -1)
+      set (CURRENT_PARAMETER ${arg})
+      set (${CURRENT_PARAMETER} "")
+    else (${ARGUMENT_IS_A_KEYWORD} GREATER -1)
+      set (${CURRENT_PARAMETER} ${${CURRENT_PARAMETER}} ${arg})
+    endif (${ARGUMENT_IS_A_KEYWORD} GREATER -1)
+  endforeach (arg)
+
+  file(TO_NATIVE_PATH ${XILINX_ISE_XFLOW} XFLOW_NATIVE)
+  file(TO_NATIVE_PATH ${XILINX_ISE_PROMGEN} PROMGEN_NATIVE)
+
+  # Copy OPT files to build tree
+  configure_file("${XILINX_OPT_DIR}/${XILINX_SYNTH_OPT_FILE}.in"
+                 "${CMAKE_CURRENT_BINARY_DIR}/${PROJ_NAME}_synth.opt")
+  configure_file("${XILINX_OPT_DIR}/${XILINX_IMPLEMENT_OPT_FILE}.in"
+                 "${CMAKE_CURRENT_BINARY_DIR}/${PROJ_NAME}_implement.opt")
+  file (COPY "${XILINX_OPT_DIR}/${XILINX_BITGEN_OPT_FILE}" DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+
+  # Prepare files used by XST (synthesis)
+  set (PRJ_FILE "${CMAKE_CURRENT_BINARY_DIR}/${PROJ_NAME}.prj")
+  file (WRITE ${PRJ_FILE} "")
+  foreach (f ${VERILOG_SOURCE})
+    file (APPEND ${PRJ_FILE} "verilog work \"${f}\"\n")
+  endforeach()
+
+  add_custom_command (OUTPUT ${PROJ_NAME}.bit
+                      COMMAND ${XFLOW_NATIVE} -p         ${FPGA_PARTNUM}
+                                              -synth     ${PROJ_NAME}_synth.opt
+                                              -implement ${PROJ_NAME}_implement.opt
+                                              -config    ${XILINX_BITGEN_OPT_FILE}
+                                              ${PROJ_NAME}.prj
+                      DEPENDS ${VERILOG_SOURCE} ${DEPENDENCIES})
+
+  # Promgen (BIT --> MCS)
+  # When using the IDE, promgen is called by impact.
+  # For now, hard-coded values such as prom size of 2048.
+  # -w            --> overwrite file
+  # -c FF         --> checksum
+  # -o <filename> --> output file name
+  # -s <size>     --> prom size (in Kbytes)
+  # -u 0000       --> load upward starting at address
+  # -spi          --> disable bit swapping for compatibility with SPI flash devices
+  # -intstyle silent
+  add_custom_command(OUTPUT "${PROJ_OUTPUT}.mcs"
+                     COMMAND ${PROMGEN_NATIVE} -intstyle silent -w -p mcs -c FF -o ${PROJ_OUTPUT} -s 2048
+                                               -u 0000 ${PROJ_NAME}.bit -spi
+                     DEPENDS  ${PROJ_NAME}.bit
+                     COMMENT "Running promgen to create ${PROJ_OUTPUT}.mcs")
+
+  add_custom_target(${PROJ_NAME} ALL
+                    DEPENDS "${PROJ_OUTPUT}.mcs")
+
+  # Additional files to clean; ${PROJ_OUTPUT}.mcs is already handled by CMake, so here we add
+  # the other generated output files (first two lines) and various log and temporary files.
+  set(XILINX_CLEAN_FILES ${PROJ_NAME}.ngc ${PROJ_NAME}.ngd ${PROJ_NAME}.pcf
+                         ${PROJ_NAME}_map.ncd ${PROJ_NAME}.ncd ${PROJ_NAME}.twr ${PROJ_NAME}.bit
+                         ${PROJ_NAME}.bgn ${PROJ_NAME}.bld ${PROJ_OUTPUT}.cfi ${PROJ_NAME}.drc
+                         ${PROJ_NAME}.lso ${PROJ_NAME}.pad ${PROJ_NAME}.par ${PROJ_NAME}.prm
+                         ${PROJ_NAME}.ptwx ${PROJ_NAME}.srp ${PROJ_NAME}.unroutes ${PROJ_NAME}.xpi
+                         ${PROJ_NAME}_map.map ${PROJ_NAME}_map.mrp ${PROJ_NAME}_map.ngm ${PROJ_NAME}_map.xrpt
+                         ${PROJ_NAME}_ngdbuild.xrpt ${PROJ_NAME}_pad.csv ${PROJ_NAME}_pad.txt
+                         ${PROJ_NAME}.xrpt ${PROJ_NAME}_summary.xml ${PROJ_NAME}_usage.xml)
+
+  set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${XILINX_CLEAN_FILES})
+
+endmacro (ise_compile_fpga)
+
+macro (ise_compile_fpga_old ...)
 
   # set all keywords and their values to ""
   set (FUNCTION_KEYWORDS
@@ -228,7 +323,7 @@ macro (ise_compile_fpga ...)
 
   set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${XILINX_CLEAN_FILES})
 
-endmacro(ise_compile_fpga)
+endmacro(ise_compile_fpga_old)
 
 macro (ise_ipcoregen ...)
 
