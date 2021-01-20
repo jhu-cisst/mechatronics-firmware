0379273c9317fb46f44060efdcd09fd2f9a2e375
Peter Kazanzides <pkaz@jhu.edu>
2021-01-20 15:07:07 -0500

    Added timing information for broadcast read

--- FPGA1394_QLA/Verilog/HubReg.v
+++ FPGA1394_QLA/Verilog/HubReg.v
@@ -3,7 +3,7 @@
 
 /*******************************************************************************
  *
- * Copyright(C) 2013-2020 ERC CISST, Johns Hopkins University.
+ * Copyright(C) 2013-2021 ERC CISST, Johns Hopkins University.
  *
  * Module: HubReg
  *
@@ -35,6 +35,9 @@ assign hub_reg_waddr = (reg_waddr[15:12]==`ADDR_HUB) && (reg_waddr[11:2] == 10'b
 // Currently, only writable register is at 0x1800
 assign hub_reg_wen = (reg_wen && hub_reg_waddr && (reg_waddr[1:0] == 2'd0));
 
+// For timing measurements. Cleared when broadcast query command received (i.e., quadlet write to 0x1800).
+reg[13:0] bcTimer;
+
 // read_index maintains an ordered list of boards in use, so that it is no longer necessary
 // for the PC to read the entire hub memory.
 reg[3:0] read_index[0:15];
@@ -42,12 +45,15 @@ reg[3:0] curIndex;
 reg[3:0] curBoard;
 reg newMask;
 
+// Indicates whether board has been updated
+reg[15:0] board_updated;
+
 wire hub_reg_raddr;
 // Hub register address space is 0x1800 - 0x1803
 assign hub_reg_raddr = (reg_raddr[15:12]==`ADDR_HUB) && (reg_raddr[11:2] == 10'b1000000000);
 
-wire[31:0] reg_rdata_mem;
-assign reg_rdata = !hub_reg_raddr ? reg_rdata_mem :
+wire[31:0] reg_rdata_hub;
+assign reg_rdata = !hub_reg_raddr ? reg_rdata_hub :
                    (reg_raddr[1:0] == 2'b00) ? {sequence, board_mask} :
                    (reg_raddr[1:0] == 2'b01) ? { read_index[ 0], read_index[ 1], read_index[ 2], read_index[ 3],
                                                  read_index[ 4], read_index[ 5], read_index[ 6], read_index[ 7] } :
@@ -57,13 +63,17 @@ assign reg_rdata = !hub_reg_raddr ? reg_rdata_mem :
 
 always @(posedge(sysclk))
 begin
+    bcTimer <=  bcTimer + 14'd1;
     if (hub_reg_wen) begin
         sequence <= reg_wdata[31:16];
         board_mask <= reg_wdata[15:0];
         curIndex <= 4'd0;
         curBoard <= 4'd0;
+        bcTimer <=  14'd0;
+        board_updated <= 16'd0;
         // Avoid board_mask==0 because that will infinite loop
-        newMask <= (reg_wdata[15:0] == 16'd0) ? 1'b0 : 1'b1;
+        // Also, check for case where new mask is equal to previous one
+        newMask <= ((reg_wdata[15:0] == 16'd0) || (reg_wdata[15:0] == board_mask)) ? 1'b0 : 1'b1;
     end
     else if (newMask) begin
        if (board_mask[curBoard]) begin
@@ -76,6 +86,9 @@ begin
              newMask <= 0;
        end
        curBoard <= curBoard + 4'd1;
+    end
+    if (hub_mem_wen) begin
+       board_updated[reg_waddr[8:5]] <= 1'b1;
     end
 end
 
@@ -95,7 +108,7 @@ localparam[8:0] QUAD_OFFSET = (9'd32-`NUM_BC_READ_QUADS);
 //
 // The implementation assumes that the hub read will start at address 0. It should actually
 // work for any start address up to the number of quadlets per board (29), but there is no
-// reason to start at any address other than 0. It also assumes that reg_waddr[15:12] will
+// reason to start at any address other than 0. It also assumes that reg_raddr[15:12] will
 // remain equal to `ADDR_HUB (1) throughout the block read operation.
 
 // Multiple of number of quadlets per entry (e.g., 29, 58, 87, ...)
@@ -121,8 +134,30 @@ wire[8:0] reg_raddr_offset;
 assign reg_raddr_offset = (reg_raddr[8:0] == multNQ) ? { reg_raddr[8:0] + offset + QUAD_OFFSET }
                                                      : { reg_raddr[8:0] + offset };
 
+// Board number being read
+wire[3:0] read_board;
+assign read_board = read_index[reg_raddr_offset[8:5]];
+
 wire[8:0] read_addr;
-assign read_addr = { read_index[reg_raddr_offset[8:5]], reg_raddr_offset[4:0] };
+assign read_addr = { read_board, reg_raddr_offset[4:0] };
+
+wire[31:0] reg_rdata_mem;
+
+// When reading first board quadlet, check whether board info has been updated since last query
+// command. If not, set bits 15:14 to 00 and return current bcTimer. If board has been updated,
+// just return contents of memory, where bits 15:14 should be 10 and the timer value should correspond
+// to when the board information was updated (see comment below).
+assign reg_rdata_hub = ((read_addr[4:0] == 5'd0) && !board_updated[read_board])
+                        ? {reg_rdata_mem[31:16], 2'b00, bcTimer }
+                        : reg_rdata_mem;
+
+// When writing first board quadlet, replace lowest 16 bits as follows:
+//   Bit 15 (1) is used to indicate that the board has been updated.
+//   Bit 14 (0) is not currently used
+//   Bits 13:0  indicate the time when the board was updated (relative to the query command)
+wire[31:0] reg_wdata_mem;
+assign reg_wdata_mem = (reg_waddr[4:0] == 5'd0) ? { reg_wdata[31:16], 2'b10, bcTimer }
+                                                : reg_wdata;
 
 //********************************* Hub memory **************************************
 // NOTE
@@ -132,7 +167,7 @@ hub_mem_gen hub_mem(
     .clka(sysclk),
     .wea(hub_mem_wen),
     .addra(reg_waddr[8:0]),
-    .dina(reg_wdata),
+    .dina(reg_wdata_mem),
     .clkb(sysclk),
     .addrb(read_addr),
     .doutb(reg_rdata_mem)
