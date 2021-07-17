eb572b6fda4e5d245555c8bd3fccd633092bb6f5
jtzhangLogan <jzhan247@jhu.edu>
2021-07-17 16:56:14 -0400

    revised implementation

--- FPGA1394_QLA/Verilog/MotorEMF.v
+++ FPGA1394_QLA/Verilog/MotorEMF.v
@@ -9,8 +9,6 @@
  *    05/07/21   Jintan Zhang          Initial revision 
 */
 
-`include "Constants.v"
-
 module MotorEMF(
     input  wire               clk,
     input  wire               val_ready,
@@ -18,8 +16,8 @@ module MotorEMF(
     input  wire        [15:0] cur_fb,
     output reg                emf_ready,
     output reg                emf_overflow,
-    output reg signed  [16:0] emf_out
-    );
+    output reg         [16:0] emf_out
+);
 
 // local wires for QLA board gain
 reg [15:0] board_gain;
@@ -28,22 +26,24 @@ reg [4 :0] board_gain_shift;
 // modified QLA board gain is 38.2, stored as 38.2 * 2^8 ~= 9779 (9779.2 raw)
 // needs to be right shifted 8 times to regain fractional value
 initial begin
-    board_gain = 9779;
+    board_gain = 9779; // 00100110_00110011
     board_gain_shift = 8;
 end
 
-// local wires for current to voltage conversion
+// local wires for commanded current to motor voltage conversion
 reg [15:0] cur2vol;
 reg [4 :0] cur2vol_shift;
 
-// conversion factor used in digital control is 1.25, stored as 1.25 * 2^8 = 320
+// DAC gain V_OUT = cur_cmd * V_REF, where V_REF = 2.5V
+// i.e. cur2vol = 
+// stored as 2.5 * 2^8 = 640
 // needs to be right shifted 8 times to regain fractional value
 initial begin
-    cur2vol = 320;
+    cur2vol = 640;     // 00000010_10000000
     cur2vol_shift = 8;
 end
 
-// local wires for motor resistence and R62
+// local wires for motor resistence
 reg [15:0] resist;
 reg [4 :0] resist_shift;
 
@@ -51,25 +51,24 @@ reg [4 :0] resist_shift;
 // stored as 7.93 * 2^8 ~= 2030 (2030.08 raw) needs to be right shifted 8 times 
 // to regain fractional value
 initial begin
-    resist = 2030;
+    resist = 2030;     // 00000111_11101110
     resist_shift = 8;
 end
 
 // local wires for intermediate/output registers
-reg signed [16:0] e_cur;
-reg signed [32:0] cur_res_prod;
-reg signed [32:0] cur2vol_prod;
-reg signed [32:0] brd_prod;
-reg signed [16:0] vol_res;
-reg signed [16:0] vol_brd;
+reg [31:0] cur_res_prod;
+reg [31:0] cur2vol_prod;
+reg [16:0] vol_res;
+reg [16:0] vol_brd;
+reg [47:0] brd_prod;
 
 // local wires for trigger signal 
 reg cur_val_ready;
 reg cur_val_ready_last;
 
 initial begin
-        cur_val_ready = 0;
-        cur_val_ready_last = 0;
+    cur_val_ready = 0;
+    cur_val_ready_last = 0;
 end
 
 // functional module
@@ -77,26 +76,37 @@ always @(posedge(clk)) begin
     // edge detection, trigger emf computation
     cur_val_ready = val_ready;
 
-    // edge detected, implements bemf = board_gain * cur2vol * (cur_cmd - cur_fb) - cur_fb * resist
+    // edge detected, implements bemf = 2 * board_gain * cur2vol * cur_cmd - cur_fb * resist
     // values in the above equation are in fixed-point representation
     if (cur_val_ready && cur_val_ready_last == 0) begin
-        // cur_cmd - cur_fb
-        e_cur = {1'b0, cur_fb} - {1'b0, cur_cmd};
 
-        // cur_fb * resist
-        cur_res_prod = cur_fb * resist;
-        vol_res = {cur_res_prod[32], cur_res_prod[22:7] >>> resist_shift};
+        cur_res_prod = ((cur_fb * resist) >>> resist_shift);
+        vol_res = {1'b0, cur_res_prod[23:8]};
 
-        // cur2vol * (cur_cmd - cur_fb)
-        cur2vol_prod = cur2vol * e_cur;
-        vol_brd = {cur2vol_prod[32], cur2vol_prod[22:7] >>> cur2vol_shift};
+        brd_prod = (cur2vol * cur_cmd * board_gain) >>> cur2vol_shift;
+        vol_brd = {1'b0, brd_prod[31:16] <<< 1};
 
-        // board_gain * cur2vol * (cur_cmd - cur_fb) - cur_fb * resist
-        brd_prod = vol_brd[15:0] * board_gain;
-        emf_out <= {vol_brd[16], brd_prod[22:7] >>> board_gain_shift - vol_res};
-
+        emf_out = vol_brd - vol_res;
+        
         // conversion complete
         emf_ready <= 1;
+
+        // case 1: cur_cmd = (0x8000)
+        // cur_fb * resist = 0.5A (0x8000) * 7.93 = 3.965, 
+        // i.e. 3.965 * 2^8 ~= 1015 (1015.04 raw) == 00001111_11011100
+        // 2 * board_gain * cur2vol * cur_cmd = 2 * 0.5A (0x8000) * 2.5 * 38.2 = 95.5
+        // i.e. 95.5 * 2^8 = 24448 == 01011111_10000000
+
+        // case 2: cur_cmd = (0xf000)
+        // cur_fb * resist = 0.9375A (0xf000) * 7.93 = 7.434, 
+        // i.e. 7.434 * 2^8 ~= 1903 (1903.2 raw) == 00000111_01101111
+        // 2 * board_gain * cur2vol * cur_cmd = 2 * 0.9375A (0xf000) * 2.5 * 38.2 ~= 179.0625
+        // i.e. 179.0625 * 2^8 = 45840 == 10110011_00010000
+
+        // max case: 
+        // i.e. vol_res = 7.93 * (0xffff) 1A = 7.93 ~= 8 -> 8 * 2^8 = 2048 == 00001000_00000000
+        //      vol_brd = 2 * 38.2 * 2.5 * (0xffff) 1A = 191 -> 191 * 2^8 = 48896 == 10111111_00000000
+        //      emf_out = vol_brd - vol_res = 191 - 8 = 183 -> 183 * 2^8 = 46848 == 10110111_00000000
     end else begin
         // wait for next valid ADC sample
         emf_ready <= 0;
@@ -107,10 +117,10 @@ always @(posedge(clk)) begin
 end
 
 // check emf overflow
-// if current error and back emf have different sign, an overflow is detected
+// if MSB of emf is set, an overflow is detected
 always @(posedge(clk)) begin
     if (emf_ready) begin
-        emf_overflow <= e_cur[16] & emf_out[16];
+        emf_overflow <= emf_out[16];
     end else begin
         emf_overflow <= 0;
     end
