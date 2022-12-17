49bd863d411e9c68375096d4c731122f9683509a
Peter Kazanzides <pkaz@jhu.edu>
2022-12-17 14:08:28 -0500

    Separate SPI sclk/conv module for driving multiple ADCs

--- FPGA1394_QLA/Verilog/CtrlAdc.v
+++ FPGA1394_QLA/Verilog/CtrlAdc.v
@@ -1,13 +1,20 @@
+/* -*- Mode: Verilog; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-   */
+/* ex: set filetype=v softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab:      */
+
 /*******************************************************************************
  *
- * Copyright(C) 2011-2020 ERC CISST, Johns Hopkins University.
+ * Copyright(C) 2011-2022 ERC CISST, Johns Hopkins University.
  *
- * This module controls access to the adc modules by selecting the data to
- * output based on the read address, and by combining the 16-bit values for
- * analog pot and motor current into a single 32-bit word.
+ * This module controls access to the adc modules.
+ * It is implemented to simultaneously interface with 4 adcs for the pots (with common
+ * sclk/conv) and with 4 adcs for the motor current feedback (with common sclk/conv).
+ *
+ * For a different number of channels (e.g., 8 instead of 4), it is easier to
+ * directly instantiate the Ltc1864SPI and Ltc1864x4 modules rather than this module.
  *
  * Revision history
  *     11/22/11    Paul Thienphrapa    Initial revision
+ *     12/17/22    Peter Kazanzides    Moved sclk/conv to Ltc1864SPI
  */
 
 // device register file offset
@@ -44,28 +51,52 @@ assign miso_pot = miso[1:4];
 assign miso_cur = miso[5:8];
 
 // pot feedback module
+
+wire pot_latch_bit;
+wire pot_latch_word;
+
+Ltc1864SPI spi_pot(
+    .clk(clkadc),
+    .sclk(sclk[1]),
+    .conv(conv[1]),
+    .OutReady(pot_ready),
+    .latch_bit(pot_latch_bit),
+    .latch_word(pot_latch_word)
+);
+
 Ltc1864x4 adc_pot(
     .clk(clkadc),
     .Out1(pot1),
     .Out2(pot2),
     .Out3(pot3),
     .Out4(pot4),
-    .OutReady(pot_ready),
-    .sclk(sclk[1]),
-    .conv(conv[1]),
+    .latch_bit(pot_latch_bit),
+    .latch_word(pot_latch_word),
     .miso(miso_pot)
 );
 
 // cur feedback module
+
+wire cur_latch_bit;
+wire cur_latch_word;
+
+Ltc1864SPI spi_cur(
+    .clk(clkadc),
+    .sclk(sclk[2]),
+    .conv(conv[2]),
+    .OutReady(cur_ready),
+    .latch_bit(cur_latch_bit),
+    .latch_word(cur_latch_word)
+);
+
 Ltc1864x4 adc_cur(
     .clk(clkadc),
     .Out1(cur1),
     .Out2(cur2),
     .Out3(cur3),
     .Out4(cur4),
-    .OutReady(cur_ready),
-    .sclk(sclk[2]),
-    .conv(conv[2]),
+    .latch_bit(cur_latch_bit),
+    .latch_word(cur_latch_word),
     .miso(miso_cur)
 );
 
