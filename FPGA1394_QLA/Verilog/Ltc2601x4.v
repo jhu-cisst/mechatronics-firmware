42d6486ee49394ab831c91c80d25131984a6898c
Peter Kazanzides <pkaz@jhu.edu>
2022-12-17 23:53:12 -0500

    Parameterized CtrlDac and Ltc2601x4 with NUM_CS (number of DAC chains) to support both QLA (NUM_CS=1) and DQLA (NUM_CS=2)

--- FPGA1394_QLA/Verilog/Ltc2601x4.v
+++ FPGA1394_QLA/Verilog/Ltc2601x4.v
@@ -34,30 +34,36 @@
  *     07/15/10                        Initial revision - MfgTest
  *     10/26/11    Paul Thienphrapa    Initial revision
  *     06/27/22    Peter Kazanzides    Modified to also support LTC2604
+ *     12/17/22    Peter Kazanzides    Parameterized with NUM_CS
  */
 
 `define SEQN_INIT 9'h100      // 9-bit counter init (from 256 to rollover)
 `define SEQN_WORD 5'b11111    // command word boundary (every 64th count)
 `define SEQN_DONE 9'h1ff      // final value of count sequence
 
-module LTC2601x4(
-    input wire clkin,         // system clock
-    input wire trig,          // initiates sample transfers
-    input wire[31:0] word,    // 32-bit command word from external memory
-    output wire[3:0] addr,    // address of command word in external memory
-    output wire sclk,         // clock to sync transfers bits with DACs
-    output reg csel,          // signal to frame transfers to the DACs
-    output wire mosi,         // serial data line into the DACs
-    output wire busy,         // indicates transfer is in progress
-    output wire flush,        // signal to flush command word to nop
-    input  wire isQuadDac     // type of DAC: 0 = 4xLTC2601, 1 = 1xLTC2604
+module LTC2601x4
+    #(parameter NUM_CS = 1)           // Number of CS (chip select) and MOSI
+(
+    input wire clkin,                 // system clock
+    input wire trig,                  // initiates sample transfers
+    input wire[(32*NUM_CS-1):0] word, // 32-bit command word(s) from external memory
+    output wire[3:0] addr,            // address of command word in external memory
+    output wire sclk,                 // clock to sync transfers bits with DACs
+    output wire[NUM_CS:1] csel,       // signal to frame transfers to the DACs
+    output wire[NUM_CS:1] mosi,       // serial data line into the DACs
+    output wire busy,                 // indicates transfer is in progress
+    output wire flush,                // signal to flush command word to nop
+    input wire[NUM_CS:1] isQuadDac    // type of DAC: 0 = 4xLTC2601, 1 = 1xLTC2604
 );
 
-    initial csel = 1'b1;
+    reg CSn;         // chip select for 4xLTC2601
+    reg CSQn;        // chip select for 1xLTC2604
+    initial CSn = 1'b1;
+    initial CSQn = 1'b1;
 
     // local wires and registers
     reg[8:0] seqn;            // 9-bit counter for sequencing operation
-    reg[31:0] data;           // capture of input command words
+    wire start;               // true if transfer is starting
     wire word_edge;           // true if transfer is at command word boundary
 
     // state machine
@@ -73,12 +79,12 @@ module LTC2601x4(
 //
 
 assign sclk = seqn[0];
-assign mosi = data[31];
-assign busy = ~csel;
+assign busy = (state == ST_LOOP) ? 1'b1 : 1'b0;
 
 assign addr = seqn[7:6] + word_edge;
 assign flush = ((seqn[5:1]==5'b01111) ? 1'b1 : 1'b0);
 assign word_edge = ((seqn[5:1]==`SEQN_WORD) ? 1'b1 : 1'b0);
+assign start = (state == ST_IDLE) ? trig : 1'b0;
 
 // state machine
 always @(posedge(clkin))
@@ -91,13 +97,12 @@ begin
     ST_IDLE: begin
         seqn <= `SEQN_INIT;              // starting state of counter
         if (trig) begin                  // write trigger detected
-            csel <= isQuadDac;           // signal start of transfer (for LTC2601)
-            data <= word;                // latch first command word
+            CSn <= 1'b0;                 // signal start of transfer (for LTC2601)
             state <= ST_LOOP;            // go to transfer loop state
         end
         else begin                       // idle state conditions
-            csel <= 1'b1;
-            data <= 0;
+            CSn <= 1'b1;
+            CSQn <= 1'b1;
             state <= ST_IDLE;
         end
     end
@@ -106,20 +111,19 @@ begin
     ST_LOOP: begin
         seqn <= seqn + 1'b1;             // counter, also creates sclk
         if (sclk == 1'b1) begin          // update data on falling sclk
-            data <= (word_edge ? word : (data<<1));
-            if (isQuadDac && word_edge) begin
-                // For LTC2604, need to deassert /CS (csel) at start of each word
-                csel <= 1'b1;
+            if (word_edge) begin
+                // For LTC2604, need to deassert /CS (CSQn) at start of each word
+                CSQn <= 1'b1;
             end
             if (seqn[5:1] == 5'b00111) begin
-                // For LTC2604, need to assert /CS (csel) 16 counts (8 SCLKs) later.
-                // For LTC2601, /CS (csel) is already asserted (0)
-                csel <= 1'b0;
+                // For LTC2604, need to assert /CS (CSQn) 16 counts (8 SCLKs) later.
+                // For LTC2601, /CS (CSn) is already asserted (0)
+                CSQn <= 1'b0;
            end
         end
         if (seqn == `SEQN_DONE) begin    // transfer complete
-            csel <= trig;                // finalize transfer, if necessary
-            data <= 0;                   // to keep mosi line clear
+            CSn <= trig;                 // finalize transfer, if necessary
+            CSQn <= trig;                // finalize transfer, if necessary
             state <= ST_IDLE;            // go back to idle state
         end
     end
@@ -131,5 +135,30 @@ begin
 
     endcase
 end
+
+genvar i;
+generate
+for (i = 1; i <= NUM_CS; i = i+1) begin : mosi_loop
+
+    // Set chip select based on DAC type
+    assign csel[i] = isQuadDac[i] ? CSQn : CSn;
+
+    reg[31:0] data;               // capture of input command words
+
+    assign mosi[i] = data[31];
+
+    always @(posedge(clkin))
+    begin
+        if (start) begin
+            data <= word[(32*i-1):(32*(i-1))]; // latch first command word
+        end
+        else if (sclk) begin
+            data <= word_edge ? word[(32*i-1):(32*(i-1))] : (data<<1);
+        end
+        // Original implementation set data to 0 at end,
+        // but this is not necessary.
+    end
+end
+endgenerate
 
 endmodule
