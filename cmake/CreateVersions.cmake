7d702d721199812364ea9dbabb3b9fe50120e8a1
Peter Kazanzides <pkaz@.jhu.edu>
2023-11-29 00:47:00 -0500

    Fixed length of GIT_SHA

--- cmake/CreateVersions.cmake
+++ cmake/CreateVersions.cmake
@@ -1,6 +1,6 @@
 # CMake module to create the specified version file
 #
-# This module is involved using "cmake -P"; the caller should first define:
+# This module is invoked using "cmake -P"; the caller should first define:
 #   SOURCE_DIR        path to the git source directory
 #   FIRMWARE_VERSION  the firmware version number
 #   OUTPUT_FILE       full path to the output file
@@ -26,7 +26,7 @@ string (SUBSTRING ${GIT_SHA_RAW} 1 7 GIT_SHA)
 file (WRITE  ${OUTPUT_FILE} "`define FW_VERSION 32'd${FIRMWARE_VERSION}        // firmware version = ${FIRMWARE_VERSION}\n\n")
 file (APPEND ${OUTPUT_FILE} "`define GIT_FW_VERSION 32'd${GIT_REV}    // git version (based on Rev tag)\n")
 file (APPEND ${OUTPUT_FILE} "`define GIT_COMMITS ${GIT_COMMITS}           // number of commits since git version tag\n")
-file (APPEND ${OUTPUT_FILE} "`define GIT_SHA 24'h${GIT_SHA}     // git SHA\n")
+file (APPEND ${OUTPUT_FILE} "`define GIT_SHA 28'h${GIT_SHA}     // git SHA\n")
 if (desc_len EQUAL 4)
   file (APPEND ${OUTPUT_FILE} "`define GIT_DIRTY 1'b1          // local changes (not committed)\n")
 else ()
